49b0f7b12d2a5597dbf8c5ef9e3004e1817e6b6c
Miguel C <mkarmona@gmail.com>
2018-09-03 09:28:21 +0100

    fix: update query documentation about overall score computation

--- src/main/resources/clickhouse/d2v2g.sql
+++ src/main/resources/clickhouse/d2v2g.sql
@@ -177,31 +177,28 @@ group by chr_id, variant_id, gene_id
 -- ORDER BY overall_score DESC
 
 -- join best genes but missing lambda array to get the top ones
--- select index_variant_id, top_genes, len_top_genes from (select index_variant_id from ot.v2d_by_stchr prewhere stid = 'NEALEUKB_50' group by index_variant_id) all inner join (select variant_id as index_variant_id, groupArray(tuple(gene_id,overall_score)) as top_genes, length(top_genes) as len_top_genes from ot.d2v2g_score_by_overall prewhere variant_id = index_variant_id and overall_score >= 0.9 group by variant_id ) using index_variant_id order by len_top_genes desc
+-- select index_variant_id, top_genes from (select index_variant_id from ot.v2d_by_stchr prewhere stid = 'NEALEUKB_50' group by index_variant_id) all left outer join (select variant_id as index_variant_id, groupArray(tuple(gene_id,overall_score)) as top_genes from ot.d2v2g_score_by_overall prewhere variant_id = index_variant_id and overall_score >= 0.9 group by variant_id ) using index_variant_id
 --
--- select
---  index_variant_id,
---  top_genes,
---  len_top_genes
--- from
---  (
---    select
---      index_variant_id
---    from ot.v2d_by_stchr
---    prewhere stid = 'NEALEUKB_50'
---    group by index_variant_id
---  )
---    all inner join
---  (
---    select
---      variant_id as index_variant_id,
---      groupArray(tuple(gene_id,overall_score)) as top_genes,
---      length(top_genes) as len_top_genes
---    from ot.d2v2g_score_by_overall
---    prewhere
---      variant_id = index_variant_id and
---      overall_score >= 0.9
---    group by variant_id
---  )
---  using index_variant_id
---  order by len_top_genes desc
+select
+ index_variant_id,
+ top_genes,
+from
+ (
+   select
+     index_variant_id
+   from ot.v2d_by_stchr
+   prewhere stid = 'NEALEUKB_50'
+   group by index_variant_id
+ )
+   all left outer join
+ (
+   select
+     variant_id as index_variant_id,
+     groupArray(tuple(gene_id,overall_score)) as top_genes,
+   from ot.d2v2g_score_by_overall
+   prewhere
+     variant_id = index_variant_id and
+     overall_score >= 0.9
+   group by variant_id
+ )
+using index_variant_id
