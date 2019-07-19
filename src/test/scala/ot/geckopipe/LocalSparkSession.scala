7bb9265e17a3f71a953c528423a4010b3409fd04
Ruslan Forostianov <ruslan@thehyve.nl>
2019-07-19 14:19:09 +0200

    Start a clean spark session for every test

--- src/test/scala/ot/geckopipe/LocalSparkSession.scala
+++ src/test/scala/ot/geckopipe/LocalSparkSession.scala
@@ -5,14 +5,17 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 class LocalSparkSessionSuite(sessionName: String, config: SparkConf = new SparkConf) extends SimpleTestSuite {
-  lazy val sparkSession: SparkSession = SparkSession.builder()
-    .appName(sessionName)
-    .config(config)
-    .master("local[*]").getOrCreate()
 
   def withSpark[T](f: SparkSession => T): T = {
+    val sparkSession: SparkSession = SparkSession.builder()
+      .appName(sessionName)
+      .config(config)
+      .master("local[*]").getOrCreate()
+
     try {
       f(sparkSession)
+    } finally {
+      sparkSession.close()
     }
   }
 }
