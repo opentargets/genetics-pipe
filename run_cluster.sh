d49e24df214a2ed7070d3f0af03b5fb79d808925
Miguel C <mkarmona@gmail.com>
2021-10-12 11:06:01 +0100

    doc: include script to run the steps in the right order

---
+++ run_cluster.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# --single-node
+gcloud beta dataproc clusters create \
+        etl-cluster-genetics-mk-96 \
+        --image-version=2.0-debian10 \
+        --region=europe-west1 \
+        --zone=europe-west1-d \
+        --master-machine-type=n1-highmem-96 \
+        --master-boot-disk-size=2000 \
+        --single-node \
+        --project=open-targets-genetics-dev \
+        --initialization-action-timeout=20m \
+        --max-idle=30m
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "variant-index"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "dictionaries"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "variant-disease-coloc"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "variant-disease"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --properties='spark.sql.autoBroadcastJoinThreshold=-1' \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "variant-gene"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "variant-gene-scored"
+
+gcloud dataproc jobs submit spark \
+     --cluster=etl-cluster-genetics-mk-96 \
+     --project=open-targets-genetics-dev \
+     --region=europe-west1 \
+     --async \
+     --jar=gs://genetics-portal-dev-data/21.06/ot-geckopipe-assembly-latest.jar \
+     -- "disease-variant-gene"
